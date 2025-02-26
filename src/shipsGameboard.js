2cc5d9ea7a3e58fa3157d6becf3bc118834af5bd
nrtmovrusm <192477100+nrtmovrusm@users.noreply.github.com>
2025-02-25 18:49:01 -0800

    Added Gameboard class

--- src/shipsGameboard.js
+++ src/shipsGameboard.js
@@ -2,6 +2,8 @@ class Ship {
   constructor(length) {
     (this.length = length), (this.noOfHits = 0);
     this.sunkStatus = false;
+    this.orientation = "horizontal";
+    this.shipCoordinates = [];
   }
 
   hit() {
@@ -14,6 +16,80 @@ class Ship {
     }
     return this.sunkStatus;
   }
+
+  changeOrientation() {
+    if (this.orientation === "horizontal") {
+      this.orientation = "vertical";
+    } else {
+      this.orientation = "horizontal";
+    }
+  }
 }
 
-export { Ship };
+class Gameboard {
+  constructor() {
+    this.board = [];
+    this.missedAttacks = [];
+    this.allShipsSunk = [];
+    this.listOfShips = [];
+  }
+
+  // where location is [x, y]
+  // need to check to see if ship is already placed there
+  placeShip(ship, [x, y]) {
+    let proposedCoordinates = [];
+
+    for (let i = 0; i < ship.length; i++) {
+      if (ship.orientation === "horizontal") {
+        proposedCoordinates.push([x + i, y]);
+      } else {
+        proposedCoordinates.push([x, y + i]);
+      }
+    }
+
+    // check if proposed coordinates for length of ship go past 10 squares and if they are already taken on the gameboard
+    for (let [proposedX, proposedY] of proposedCoordinates) {
+      if (proposedX > 10 || proposedY > 10) {
+        return false;
+      }
+      let exists = this.board.some(
+        ([xCoord, yCoord]) => xCoord === proposedX && yCoord === proposedY,
+      );
+      if (exists) {
+        return false;
+      }
+    }
+    // only allow coordinates if previous loop is true (i.e. no ship already exists in the coordinates)
+    ship.shipCoordinates.push(...proposedCoordinates);
+    this.board.push(...ship.shipCoordinates);
+    this.listOfShips.push(ship);
+  }
+
+  receiveAttack([x, y]) {
+    // look in listOfShips to see which one has coordinates matching
+    for (let i = 0; i < this.listOfShips.length; i++) {
+      let ship = this.listOfShips[i];
+      let shipCoordinatesArray = ship.shipCoordinates;
+      let hit = shipCoordinatesArray.some(
+        ([shipX, shipY]) => shipX === x && shipY === y,
+      );
+      if (hit) {
+        ship.hit();
+        return;
+      }
+    }
+    this.missedAttacks.push([x, y]);
+  }
+
+  allSunk() {
+    for (let i = 0; i < this.listOfShips.length; i++) {
+      let ship = this.listOfShips[i];
+      this.allShipsSunk.push(ship.isSunk());
+    }
+
+    let allSunk = this.allShipsSunk.every((value) => value === true);
+    return allSunk;
+  }
+}
+
+export { Ship, Gameboard };
--- src/shipsGameboard.test.js
+++ src/shipsGameboard.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "./shipsGameboard";
+import { Ship, Gameboard } from "./shipsGameboard";
 
 describe("class Ship", () => {
   let ship;
@@ -29,5 +29,95 @@ describe("class Ship", () => {
   test("return true if ship with length 3 hit 3 times", () => {
     ship.noOfHits = 3;
     expect(ship.isSunk()).toBe(true);
+  });
+
+  test("change orientation of ship that has default horizontal orientation", () => {
+    ship.changeOrientation();
+    expect(ship.orientation).toBe("vertical");
+  });
+});
+
+describe("Gameboard", () => {
+  let gameboard;
+  let ship;
+
+  beforeEach(() => {
+    gameboard = new Gameboard();
+    ship = new Ship(3);
+  });
+
+  test("placing ships on initial turn across multiple squares", () => {
+    gameboard.placeShip(ship, [3, 2]);
+    expect(ship.shipCoordinates).toEqual([
+      [3, 2],
+      [4, 2],
+      [5, 2],
+    ]);
+  });
+
+  test("gameboard has coordinates from ship placement", () => {
+    gameboard.placeShip(ship, [3, 2]);
+    expect(gameboard.board).toEqual([
+      [3, 2],
+      [4, 2],
+      [5, 2],
+    ]);
+  });
+
+  test("gameboard with multiple ships adds to board array", () => {
+    let ship2 = new Ship(2);
+    gameboard.placeShip(ship, [3, 2]);
+    gameboard.placeShip(ship2, [3, 5]);
+    expect(gameboard.board).toEqual([
+      [3, 2],
+      [4, 2],
+      [5, 2],
+      [3, 5],
+      [4, 5],
+    ]);
+  });
+
+  test("cannot place second ship that clashes with an already placed ship", () => {
+    let ship2 = new Ship(2);
+    gameboard.placeShip(ship, [3, 2]);
+    gameboard.placeShip(ship2, [4, 2]);
+    expect(gameboard.board).toEqual([
+      [3, 2],
+      [4, 2],
+      [5, 2],
+    ]);
+  });
+
+  test("cannot directly place ship beyond 10 squares", () => {
+    expect(gameboard.placeShip(ship, [11, 2])).toBe(false);
+  });
+
+  test("cannot implicitly place ship beyond 10 squares", () => {
+    expect(gameboard.placeShip(ship, [9, 2])).toBe(false);
+  });
+
+  test("[x,y] matching ship placement leads to hit for correct ship", () => {
+    let ship2 = new Ship(2);
+    gameboard.placeShip(ship, [3, 2]);
+    gameboard.placeShip(ship2, [3, 5]);
+    gameboard.receiveAttack([4, 2]);
+    expect(ship.noOfHits).toEqual(1);
+  });
+
+  test("[x,y] NOT matching ships gets added to missed attacks array", () => {
+    let ship2 = new Ship(2);
+    gameboard.placeShip(ship, [3, 2]);
+    gameboard.placeShip(ship2, [3, 5]);
+    gameboard.receiveAttack([6, 2]);
+    expect(gameboard.missedAttacks).toEqual([[6, 2]]);
+  });
+
+  test("allSunk returns true if all ships have been sunk", () => {
+    let ship2 = new Ship(2);
+    gameboard.placeShip(ship, [3, 2]);
+    gameboard.placeShip(ship2, [3, 5]);
+    ship.sunkStatus = true;
+    ship2.sunkStatus = true;
+    expect(gameboard.allSunk()).toBe(true);
   });
 });
